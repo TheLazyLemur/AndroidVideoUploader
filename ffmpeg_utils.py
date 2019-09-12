c5cbbb2325b37e490976e64a6eee8a2a260c60ae
Dan Rousseau <Danrousseau@protonmail.com>
2019-09-12 16:07:59 +0200

    increased time

--- ffmpeg_utils.py
+++ ffmpeg_utils.py
@@ -3,5 +3,5 @@ import os
 
 
 def make_thumbnail(cur_dir, video_title):
-    subprocess.call(f"./ffmpeg/bin/ffmpeg.exe -i {video_title}.mp4 -ss 00:00:01.000 -vframes 1 {video_title}.png")
+    subprocess.call(f"./ffmpeg/bin/ffmpeg.exe -i {video_title}.mp4 -ss 00:00:10.000 -vframes 1 {video_title}.png")
     print(os.listdir(cur_dir))
